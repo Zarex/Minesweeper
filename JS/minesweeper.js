f4404d5a1bc8eae7e093b670b4613177d807f21e
dunstad <anthony00.blount00@gmail.com>
2010-10-21 16:57:51 -0500

    Made surroundings work as a method of board, added board.cellAt, made a few functions accept two ints instead of a list, it looks nicer that way.

--- JS/minesweeper.js
+++ JS/minesweeper.js
@@ -1,21 +1,66 @@
 function Board(width, height) {
 	
-	board = [];
+	var tempBoard = this; // Many methods need access to this inside themselves.
+	 
+	this.board = [];
 	
 	for (var y = 0; y < height; y++) {
 		
-		board.push([]);
+		this.board.push([]);
 		
 		for (var x = 0; x < width; x++) {
 			
-			board[y].push([]);
+			this.board[y].push([]);
 		
 		}
 	
 	}
+	this.contains= function (loc0, loc1)
+	{
+		if(tempBoard.board[loc0] === undefined)
+			return false;
+		if(tempBoard.board[loc0][loc1] === undefined)
+			return false;
+		return true;
+	};
 	
-	return board;
-
+	this.cellAt = function(loc0, loc1) {
+		
+		if (tempBoard.contains(loc0, loc1)) {
+			
+			return tempBoard.board[loc0][loc1];
+			
+		}
+		
+		return false;
+		
+	}
+	
+	
+	this.surroundings = function (center_cell){
+	
+		var surroundingCells = [];
+		
+		for(var i = -1; i < 2; i++)
+		{
+			for(var j = -1; j < 2; j++)
+			{	
+				new_cell = tempBoard.cellAt(center_cell.loc[0] + i,
+											center_cell.loc[1] + j);
+				
+				// Don't include spaces off the board or yourself.
+				if (!(new_cell) || i == 0 && j == 0) continue;
+				
+				surroundingCells.push(new_cell);
+					
+			}
+			
+		}
+		
+		return surroundingCells;
+		
+	};
+	
 }
 
 function Cell (mine, loc, revealed)
@@ -27,35 +72,6 @@ function Cell (mine, loc, revealed)
 		this.revealed = false;
 		
 	this.loc = [parseInt(loc/width), loc % width];
-	
-	var tempLoc = [this.loc[0],this.loc[1]];
-	this.surroundings = function (){
-		var surroundingCells = [];
-		for(var i = -1; i < 2; i++)
-		{
-			for(var j = -1; j < 2; j++)
-			{
-				
-				/* This could probably be cleaner. */
-				// Skip spaces that aren't on the board.
-				try {
-					var cell = board[tempLoc[0]+i][tempLoc[1]+j];
-				}
-				
-				catch(e) {
-					continue;
-				}
-				
-				// Don't include yourself or spaces off the board.
-				if (cell === undefined ||
-					cell.loc[0] == tempLoc[0] && cell.loc[1] == tempLoc[1]) continue;
-				
-				surroundingCells.push(cell);
-					
-			}
-		}
-		return surroundingCells;
-	};
 	
 }
 
